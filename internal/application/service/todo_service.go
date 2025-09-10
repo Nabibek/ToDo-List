c4927964f0988230008eac3714fa45ed705ad84d
nabibek <nabibiek@mail.ru>
2025-09-11 04:33:26 +0500

    feat:service

---
+++ internal/application/service/todo_service.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"ToDo-List/internal/core/domain"
+	"ToDo-List/internal/core/ports"
+
+	"github.com/google/uuid"
+)
+
+type TodoService struct {
+	repo ports.PostgreRepo
+}
+
+func NewToDoService(repo ports.PostgreRepo) ports.ToDoService {
+	return &TodoService{repo: repo}
+}
+
+func (s *TodoService) CreateTodo(ctx context.Context, todo domain.ToDo) (domain.ToDo, error) {
+	todo.Id = uuid.NewString()
+	todo.CreatedAt = time.Now()
+	todo.UpdatedAt = time.Now()
+
+	return s.repo.CreateTodo(ctx, todo)
+}
+
+func (s *TodoService) GetAllTodos(ctx context.Context) ([]domain.ToDo, error) {
+	return s.repo.GetAllTodos(ctx)
+}
+
+func (s *TodoService) GetTodoById(ctx context.Context, id string) (domain.ToDo, error) {
+	return s.repo.GetTodoById(ctx, id)
+}
+
+func (s *TodoService) UpdateTodo(ctx context.Context, todo domain.ToDo) error {
+	todo.UpdatedAt = time.Now()
+	return s.repo.UpdateTodo(ctx, todo)
+}
+
+func (s *TodoService) DeleteTodo(ctx context.Context, id string) error {
+	return s.repo.DeleteTodoById(ctx, id)
+}
---
+++ internal/core/ports/service.go
@@ -0,0 +1,15 @@
+package ports
+
+import (
+	"context"
+
+	"ToDo-List/internal/core/domain"
+)
+
+type ToDoService interface {
+	CreateTodo(ctx context.Context, todo domain.ToDo) (domain.ToDo, error)
+	GetAllTodos(ctx context.Context) ([]domain.ToDo, error)
+	GetTodoById(ctx context.Context, id string) (domain.ToDo, error)
+	UpdateTodo(ctx context.Context, todo domain.ToDo) error
+	DeleteTodo(ctx context.Context, id string) error
+}
